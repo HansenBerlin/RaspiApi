c2002da28543141e1a9c7d2bc73aa53935a78263
Hannes Roever <h.roever@zertificon.com>
2022-12-26 01:48:14 +0100

    -

--- RaspiApi/Program.cs
+++ RaspiApi/Program.cs
@@ -1,9 +1,6 @@
-using System.Collections;
-using System.Device.Gpio;
 using System.Drawing;
 using rpi_ws281x;
 
-int[] allowedPins = { 5, 6, 13, 18, 23, 24, 12, 33, 32 };
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,45 +14,74 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
-app.MapGet("/", () => "Minimal User Data API");
+const int count = 145;
+var settings = Settings.CreateDefaultSettings();
+var controller = settings.AddController(count, Pin.Gpio18, StripType.WS2812_STRIP);
+var rpi = new WS281x(settings);
 
-
-app.MapGet("/freq/{hex}/{led:int}", (string hex, int led) =>
+app.MapPost("/set", (Dictionary<int, string> json) =>
 {
-    var settings = Settings.CreateDefaultSettings();
-    var controller = settings.AddController(300, Pin.Gpio18, StripType.WS2812_STRIP);
-    Color color = ColorTranslator.FromHtml("#" + hex);
-    using (var rpi = new WS281x(settings))
+    foreach (var kv in json)
     {
-        controller.SetLED(led, color);
+        var color = ColorTranslator.FromHtml(kv.Value);
+        controller.SetLED(kv.Key, color);
+    }
+    rpi.Render();
+});
+
+app.MapGet("/active", () =>
+{
+    Dictionary<int, bool> activeLeds = new();
+    var leds = controller.LEDs.ToList();
+    for (int i = 0; i < leds.Count; i++)
+    {
+        bool isOn = ColorTranslator.ToHtml(leds[i].Color) != "#000000";
+        activeLeds.Add(i, isOn);
+    }
+
+    return activeLeds;
+});
+
+app.MapGet("/colors", () =>
+{
+    Dictionary<int, string> activeLeds = new();
+    var leds = controller.LEDs.ToList();
+    for (int i = 0; i < leds.Count; i++)
+    {
+        var color = ColorTranslator.ToHtml(leds[i].Color);
+        activeLeds.Add(i, color);
+    }
+
+    return activeLeds;
+});
+
+app.MapPost("/shuffle", async (Shuffle json) =>
+{
+    var color = ColorTranslator.FromHtml(json.Color);
+    var range = Enumerable.Range(0, 145).ToList();
+    Random random = new Random();
+    range = range.OrderBy(x => random.Next()).ToList();
+
+    foreach (var rn in range)
+    {
+        controller.SetLED(rn, color);
         rpi.Render();
+        await Task.Delay(json.Delay);
     }
 });
 
-
-void Open(int pin)
+app.MapPost("/alloff", () =>
 {
-    using var controller = new GpioController();
-    controller.OpenPin(pin, PinMode.Output);
-    controller.Write(pin, PinValue.High);
-    
-    
-    //controller.ClosePin(pin);
-}
-
-void Close(int pin)
-{
-    using var controller = new GpioController();
-    controller.OpenPin(pin, PinMode.Output);
-    controller.Write(pin, PinValue.Low);
-    //controller.ClosePin(pin);
-}
-
-bool IsAllowed(int pin)
-{
-    return allowedPins.Contains(pin);
-}
-
+    var color = ColorTranslator.FromHtml("#" + 000000);
+    for(int i = 0; i < count; i++)
+    {
+        controller.SetLED(i, color);
+    }
+    rpi.Render();
+});
 
 app.Run();
+
+public record Shuffle(int Delay, string Color);
+
 
